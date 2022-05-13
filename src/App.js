39fbfc0bcb3c56fc103953ff9d88a8df35b87dff
LazyMonkey <75183320+ItzLazyTechie@users.noreply.github.com>
2022-05-13 21:53:55 +0530

    ChartJS-Code

--- src/App.js
+++ src/App.js
@@ -1,33 +1,122 @@
-import React, { useState } from 'react';
-
-import Toast from 'react-bootstrap/Toast';
+import React from 'react';
 import Container from 'react-bootstrap/Container';
 
-import './App.css';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
 
-const ExampleToast = ({ children }) => {
-  const [show, toggleShow] = useState(true);
+import ChartDataLabels from 'chartjs-plugin-datalabels';
+import { Bar } from 'react-chartjs-2';
 
-  return (
-    <Toast show={show} onClose={() => toggleShow(!show)}>
-      <Toast.Header>
-        <strong className="mr-auto">React-Bootstrap</strong>
-      </Toast.Header>
-      <Toast.Body>{children}</Toast.Body>
-    </Toast>
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+  ChartDataLabels
+);
+
+const ExampleToast2 = () => {
+
+  
+ const options = {
+  plugins: {
+    title: {
+      display: true,
+      text: 'A - Ben Allocation Percentage',
+    },
+    datalabels: {
+      display: true,
+      color: "white",
+      plugins: {
+        datalabels: {
+            formatter: (value, ctx) => {
+                let sum = 0;
+                let dataArr = ctx.chart.data.datasets[0].data;
+                dataArr.map(data => {
+                    sum += data;
+                });
+                let percentage = (value*100 / sum).toFixed(2)+"%";
+                return percentage;
+            },
+            color: '#fff',
+        }
+    }
+   }
+  },
+  responsive: true,
+  maintainAspectRatio: false,
+  scales: {
+    x: {
+      stacked: true,
+      grid: {
+        display: false
+      }
+      
+    },
+    y: {
+      stacked: true,
+      grid: {
+        display: false
+      },
+      ticks: {   
+        min: 0,
+        max: 100,
+        callback: function(value){return value+ "%"}
+     }
+    },
+  },
+};
+
+const labels = ['A1', 'A2', 'A3', 'A4', 'A5'];
+
+ const data = {
+  labels,
+  datasets: [
+    {
+      label: 'Ben1',
+      data: [10,5,3,17,7],
+      backgroundColor:  '#caf270',
+    },
+    {
+      label: 'Ben2',
+      data: [5,10,17,27,23],
+      backgroundColor: '#45c490',
+    },
+    {
+      label: 'Ben3',
+      data: [20,5,20,2,15] ,
+      backgroundColor: '#008d93',
+    },
+    {
+      label: 'Ben4',
+      data: [5,20,10,4] ,
+      backgroundColor: '#2e5468',
+    },
+  ],
+};
+ 
+ return (
+    <div style={{width:"600px",height:"400px"}}>
+      <Bar options={options} data={data} />
+    </div>
   );
 };
 
 const App = () => (
-  <Container className="p-3">
-    <Container className="p-5 mb-4 bg-light rounded-3">
+  <Container>
+    <Container>
       <h1 className="header">Welcome To React-Bootstrap</h1>
-      <ExampleToast>
-        We now have Toasts
-        <span role="img" aria-label="tada">
-          🎉
-        </span>
-      </ExampleToast>
+      <ExampleToast2>
+      </ExampleToast2>
     </Container>
   </Container>
 );
