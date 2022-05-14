d9762c9b97c54fe858501d64996802b1f9ed0fc8
LazyMonkey <75183320+ItzLazyTechie@users.noreply.github.com>
2022-05-14 12:05:39 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -1,124 +1,49 @@
-import React from 'react';
+
 import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import DisplayPieChart from './DisplayPieChart';
+import DisplayBarChart from './DisplayBarChart';
 
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  Title,
-  Tooltip,
-  Legend,
-} from 'chart.js';
-
-import ChartDataLabels from 'chartjs-plugin-datalabels';
-import { Bar } from 'react-chartjs-2';
-
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  Title,
-  Tooltip,
-  Legend,
-  ChartDataLabels
-);
-
-const ExampleToast2 = () => {
-
-  
- const options = {
-  plugins: {
-    title: {
-      display: true,
-      text: 'A - Ben Allocation Percentage',
-    },
-    datalabels: {
-      display: true,
-      color: "white",
-      plugins: {
-        datalabels: {
-            formatter: (value, ctx) => {
-                let sum = 0;
-                let dataArr = ctx.chart.data.datasets[0].data;
-                dataArr.map(data => {
-                    sum += data;
-                });
-                let percentage = (value*100 / sum).toFixed(2)+"%";
-                return percentage;
-            },
-            color: '#fff',
-        }
-    }
-   }
-  },
-  responsive: true,
-  maintainAspectRatio: false,
-  scales: {
-    x: {
-      stacked: true,
-      grid: {
-        display: false
-      }
-      
-    },
-    y: {
-      stacked: true,
-      grid: {
-        display: false
-      },
-      ticks: {   
-        min: 0,
-        max: 100,
-        callback: function(value){return value+ "%"}
-     }
-    },
-  },
-};
-
-const labels = ['A1', 'A2', 'A3', 'A4', 'A5'];
-
- const data = {
-  labels,
-  datasets: [
-    {
-      label: 'Ben1',
-      data: [10,5,3,17,7],
-      backgroundColor:  '#caf270',
-    },
-    {
-      label: 'Ben2',
-      data: [5,10,17,27,23],
-      backgroundColor: '#45c490',
-    },
-    {
-      label: 'Ben3',
-      data: [20,5,20,2,15] ,
-      backgroundColor: '#008d93',
-    },
-    {
-      label: 'Ben4',
-      data: [5,20,10,4] ,
-      backgroundColor: '#2e5468',
-    },
-  ],
-};
- 
- return (
-    <div style={{width:"600px",height:"400px"}}>
-      <Bar options={options} data={data} />
-    </div>
-  );
-};
+import './App.css';
 
 const App = () => (
-  <Container>
+  <>
     <Container>
-      <h1 className="header">Welcome To React-Bootstrap</h1>
-      <ExampleToast2>
-      </ExampleToast2>
+      <h5>My Info</h5>
+
+      <Row className="accordion-body">
+        <Col md={3}>
+          <b> NAME : </b>
+          <span style={{ marginLeft: "10px" }}> JOHN WILLIAMS </span>
+        </Col>
+
+        <Col md={3}>
+          <b style={{ marginLeft: "80px" }}>AGE : </b>
+          <span style={{ marginLeft: "10px" }}> 25 </span>
+        </Col>
+
+        <Col md={6}>
+          <b>ADDRESS : </b>
+          <span style={{ marginLeft: "10px" }}> 299 MILLER ISLANDS APT. 403 OLSONCHESTER, NM 67179. </span>
+        </Col>
+      </Row>
+
+      <h5>Assets - Beneficiary Wise Allocation</h5>
+      <Row className="accordion-body">
+        <Col md={6}><DisplayBarChart></DisplayBarChart></Col>
+        <Col md={6} style={{ paddingLeft: "110px" }}><DisplayPieChart></DisplayPieChart></Col>
+      </Row>
+
+      <h5>My Assets : </h5>
+      <Row className="accordion-body">
+        <Col md={6}><DisplayBarChart></DisplayBarChart></Col>
+        <Col md={6} style={{ paddingLeft: "110px" }}><DisplayPieChart></DisplayPieChart></Col>
+      </Row>
+
     </Container>
-  </Container>
+
+  </>
 );
 
 export default App;
